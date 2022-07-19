a074a28cdad12d1491f06b6e02c9e91e4f72afa0
Julius Stopforth <jjstopforth@gmail.com>
2022-07-19 18:19:47 +0200

    Add autostart config to awesome

--- awesome/.config/awesome/autostart.sh
+++ awesome/.config/awesome/autostart.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 run() {
-  if ! pgrep -f "$1" ;
-  then
-    "$@"&
-  fi
+    if ! pgrep -f "$1" ;
+    then
+        "$@"&
+    fi
 }
 
-run "emacs --daemon"
 run "redshift-gtk"
-run "eval $(ssh-agent)"
-
+run "picom"
 
