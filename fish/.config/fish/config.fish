ea956254019baded065496483bdcbd13bf52868d
Julius Stopforth <jjstopforth@gmail.com>
2023-04-30 09:37:57 +0200

    add bang functions to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,3 +21,28 @@ and not set -q TMUX
         tmux new-session -s home
     end
 end
+
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
